d55f245ca21a389a8dd1246afdf7cbb23fe5832f
MeleNik20 <andres103107@gmail.com>
2024-12-28 21:39:50 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,81 @@
+var numero = "";
+var numMuestra = "";
+var numero1 = 0;
+var numero2 = 0;
+var resultado = 0;
+var operador = "";
+
+function Sumar() {
+    numero1 = Number(numero);
+    operador = "+";
+    numero = "";
+    Escribir('+');
+}
+
+function Resta() {
+    numero1 = Number(numero);
+    operador = "-";
+    numero = "";
+    Escribir('-');
+}
+
+function Multiplicar() {
+    numero1 = Number(numero);
+    operador = "*";
+    numero = "";
+    Escribir('*');
+}
+
+function Dividir() {
+    numero1 = Number(numero);
+    operador = "/";
+    numero = "";
+    Escribir('/');
+}
+
+function Escribir(P_Numero) {
+    if (P_Numero === "+" || P_Numero === "-" || P_Numero === "*" || P_Numero === "/") {
+        // Si es un operador, se agrega al display
+        numMuestra = numMuestra + P_Numero;
+    } else {
+        // Si es un número, se concatena al número actual
+        numero = numero + P_Numero;
+        numMuestra = numMuestra + P_Numero;
+    }
+
+    num1.value = numMuestra;
+}
+
+function limpiar() {
+    numero = "";
+    numMuestra = "";
+    numero1 = 0;
+    numero2 = 0;
+    operador = "";
+    document.getElementById("num1").value = "";
+}
+
+function Resultado() {
+    numero2 = Number(numero);
+    if (operador === "+") {
+        resultado = numero1 + numero2;
+    } else if (operador === "-") {
+        resultado = numero1 - numero2;
+    } else if (operador === "*") {
+        resultado = numero1 * numero2;
+    } else if (operador === "/") {
+        if (numero2 !== 0) {
+            resultado = numero1 / numero2;
+        } else {
+            resultado = "Error";
+        }
+    }
+    num1.value = resultado;
+    // Reiniciar todo para el siguiente cálculo
+    numero = "";
+    numMuestra = "";
+    numero1 = 0;
+    numero2 = 0;
+    operador = "";
+}
+
